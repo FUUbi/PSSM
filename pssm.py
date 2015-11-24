98528434e224be1b62202c77d49caf869b17e4fb
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2015-11-24 11:19:53 +0100

    ist das pssm

--- pssm.py
+++ pssm.py
@@ -11,7 +11,8 @@ for seq_record in SeqIO.parse("data/test_neg.txt", "fasta"):
 
 # wmm
 m = motifs.create(instance)
-pwm = m.counts.normalize(pseudocounts=0.5)
+pwm = m.pwm
+print(pwm)
 
 # You can also directly access columns of the counts matrix
 m.counts[:,3]
@@ -20,7 +21,8 @@ m.counts[:,3]
 m.counts['A']
 
 # pssm
-pssm =pwm.log_odds()
+pssm = pwm.log_odds()
+print pssm['A',3]
 
 class Pssm:
     def __init__(self):
