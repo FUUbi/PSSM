ce967eae6df888c39b938798733214011858b219
FUUbi <fabriparrillo@hotmail.com>
2015-11-24 13:01:25 +0100

    calc scores

--- pssm.py
+++ pssm.py
@@ -4,6 +4,7 @@ from Bio.Seq import Seq
 
 # http://biopython-cn.readthedocs.org/en/latest/en/chr14.html
 
+
 instance = []
 for seq_record in SeqIO.parse("data/test_neg.txt", "fasta"):
     dna_seq = Seq(str(seq_record.seq).upper())
@@ -28,27 +29,35 @@ print pssm
 class Pssm:
     def __init__(self):
         self.spliceSite = None
-
         self.background = None
 
-    def calclateScore(self, motiv):
-        score = 0
-        for i in range(0, len(motiv)):
-            score += self.pssm[i][motiv[i]]
+        self.seqPos = None
+        self.seqNeg = None
 
+        self.posScores = None
+        self.negScores = None
 
-#        for p in self.pssm:
-#           score += p[motiv[0]]
-#           motiv = motiv[1:]
+    def calculatePosScore(self):
+        self.posScores = []
+        for seq in self.seqPos:
+            score = 0
+            for i in range(0, len(seq)):
+                score += self.spliceSite[seq[i], i]
+            self.posScores.append(score)
 
-        return score
+    def calculateNegScore(self):
+        self.negScores = []
+        for seq in self.seqNeg:
+            score = 0
+            for i in range(0, len(seq)):
+                score += self.spliceSite[seq[i], i]
+            self.negScores.append(score)
 
     def setSpliceSite(self, filePath):
         self.spliceSite = self.calcutaltePssm(filePath)
 
     def setBackground(self, filePath):
         self.background = self.calcutaltePssm(filePath)
-
 
     def calcutaltePssm(self, filePath):
         instance = []
@@ -57,17 +66,36 @@ class Pssm:
             instance.append(dna_seq)
         # wmm
         m = motifs.create(instance)
+
         pwm = m.counts.normalize(pseudocounts=0.5)
         return pwm.log_odds()       # pssm
 
+    def loadSeq(self, filePath):
+        instance = []
+        for seq_record in SeqIO.parse(filePath, "fasta"):
+            dna_seq = Seq(str(seq_record.seq).upper())
+            instance.append(dna_seq)
 
+        return instance
+
+    def setSeqPos(self, filePath):
+        self.seqPos = self.loadSeq(filePath)
+
+    def setSeqNeg(self, filePath):
+        self.seqNeg = self.loadSeq(filePath)
 
 if __name__  == "__main__":
     pssm = Pssm()
     pssm.setSpliceSite("data/train_pos.txt")
     pssm.setBackground("data/train_neg.txt")
-
+    pssm.setSeqPos("data/test_pos.txt")
+    pssm.setSeqNeg("data/test_neg.txt")
 
     print(pssm.background)
     print(pssm.spliceSite)
+
+    pssm.calculateNegScore()
+    pssm.calculatePosScore()
+    for i in range(0, 10):
+        print("neg" + str(pssm.negScores[i]) + "   pos " + str(pssm.posScores[i]))
 
