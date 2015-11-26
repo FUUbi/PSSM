804e8c65def17f52695bf61a9e58a890d1a5fad1
Steffen685 <steffengriesch@gmail.com>
2015-11-26 22:24:19 +0100

    ROC Curve

--- pssm.py
+++ pssm.py
@@ -81,12 +81,24 @@ if __name__  == "__main__":
     #########################################################################################################
     #########################################################################################################
     #########################################################################################################
+    # negScoresSorted = list()
+    # posScoresSorted = list()
+    #
+    # negScoresSorted = (3.19113753652, 1.76896238158, 1.10029167012, 0.25975834748, -1.44403621830, -2.18898610220, -2.60898178181, -3.13453393946, -3.22828617301, -4.77617608561)
+    #
+    # posScoresSorted = (6.84946860220, 5.21223598584, 5.05555992006, 4.69326858755, 4.16360951066, 3.68749250459, 3.51298798280, 2.45355113003, 1.46175619017, 0.363484753632)
+    #
+    # print(negScoresSorted)
+    # print(posScoresSorted)
+    #
+    # totalNegScores = len(negScoresSorted)
+    # totalPosScores = len(posScoresSorted)
 
-    ## Total Scores
+    # Total Scores
     totalPosScores = len(pssm.posScores)
     totalNegScores = len(pssm.negScores)
 
-    ## Sort the Scores
+    # Sort the Scores
     posScoresSorted = sorted(pssm.posScores, reverse=True)
     negScoresSorted = sorted(pssm.negScores, reverse=True)
 
@@ -97,49 +109,70 @@ if __name__  == "__main__":
     def probCutOff(start, stop, step):
         pronCutOffList = list()
         i = start
-        while i < stop:
+        while i <= stop:
+            i = round(i, 1)
             pronCutOffList.append(i)
             i+= step
         return pronCutOffList
 
-    cutOff = probCutOff(0.0, 8.0, 0.1)
 
     ## The TruePossitives "TP" for a Probable Cut Off Value,ScoreList "posScores" must be sorted
     def truePositives(pco, posScores):
+        tpList = list()
         pco = pco
+        posScores = posScores
         value = 0
-        for i in range(len(posScores)):
-            if posScores[i] > pco:
-                value +=1
-        return value
+        for i in range(len(pco)):
+            for x in range(len(posScores)):
+                if posScores[x] > pco[i]:
+                    value +=1
+            tpList.append(value)
+            value = 0
+        return tpList
+
 
     ## The TrueNegatives "TN" for a Probable Cut Off Value, ScoreList "negScores" must be sorted
     def trueNegatives(pco, negScores):
+        tnList = list()
         poc = pco
+        negScores = negScores
         value = 0
-        for i in range(len(negScores)):
-            if negScores[i] > poc:
-                value += 1
-        value = len(negScores)-value
-        return value
+        for i in range(len(pco)):
+            for x in range(len(negScores)):
+                if negScores[x] < poc[i]:
+                    value +=1
+            tnList.append(value)
+            value = 0
+        return tnList
 
     ## The TN-Rate "Specifity" i = length of the tnList, tnList = list of TN Values, totalNeg = total negative values
-    def tnRate(i, tnList, totalNeg):
+    def tnRate(tnList, totalNeg):
+        tnRateList = list()
         tnList = tnList
-        value = (tnList[i] / totalNeg)*100
-        return value
+        totalNeg = totalNeg
+        for i in range(len(tnList)):
+            value = float(tnList[i]) / totalNeg
+            tnRateList.append(value)
+        return tnRateList
 
     ## The FP-Rate "1-TN Rate"
-    def fpRate(i, tnRateList):
+    def fpRate(tnRateList):
+        fpRateList = list()
         tnRateList = tnRateList
-        value = 100 - tnRateList[i]
-        return value
+        for i in range(len(tnRateList)):
+            value = float(1 - tnRateList[i])
+            fpRateList.append(value)
+        return fpRateList
 
     ## The TP-Rate "Sensivity" tpList = list of TP Values, totalPos = total positive values
-    def tpRate(i, tpList, totalPos):
+    def tpRate(tpList, totalPos):
+        tpRateList = list()
         tpList = tpList
-        value = (tpList[i] / totalPos)*100
-        return value
+        totalPos = totalPos
+        for i in range(len(tpList)):
+            value = float(tpList[i]) / totalPos
+            tpRateList.append(value)
+        return tpRateList
 
     ##########################################################################################################
     ##########################################################################################################
@@ -157,7 +190,7 @@ if __name__  == "__main__":
     ##  Iterate through the tnList using the tnRate() function and generate a List containing the values    ##
     ##  for each value                                                                                      ##
     ##                                                                                                      ##
-    ##  Iterate through the tnRateList using the fpPate() function and generate a List containing           ##
+    ##  Iterate through the tnRateList using the fpRate() function and generate a List containing           ##
     ##  the value for each value                                                                            ##
     ##                                                                                                      ##
     ##  Iterate through the tpList using the tpRate() function and generate a List containing the values    ##
@@ -171,3 +204,48 @@ if __name__  == "__main__":
     ##                                                                                                      ##
     ##########################################################################################################
     ##########################################################################################################
+
+
+    def rocCurve(pcoStart, pcoEnd, pcoStep, sortedPos, sortedNeg):
+        print("generatin the Data for the ROC Curve")
+        print("If this takes more than 15 sec. please go to www.apple.com and by a daaaaaamn fucking MacBook Pro")
+
+        pcoStart = pcoStart
+        pcoEnd = pcoEnd
+        pcoStep = pcoStep
+        posScoresSorted = sortedPos
+        negScoresSorted = sortedNeg
+
+        probCutOffList = probCutOff(pcoStart, pcoEnd, pcoStep)
+        tpList = truePositives(probCutOffList, posScoresSorted)
+        tnList = trueNegatives(probCutOffList, negScoresSorted)
+        tnRateList = tnRate(tnList, totalNegScores)
+        fpRateList = fpRate(tnRateList)
+        tpRateList = tpRate(tpList, totalPosScores)
+
+        # print(probCutOffList)
+        #
+        # print(tpList)
+        #
+        # print(tnList)
+        #
+        # print(tnRateList)
+        #
+        # print(fpRateList)
+        #
+        # print(tpRateList)
+
+        fp = fpRateList
+        tp = tpRateList
+
+        plt.plot(fp, tp, color="purple")
+        plt.show()
+
+
+
+
+
+
+    rocCurve(-1.0, 4.0, 0.1, posScoresSorted, negScoresSorted)
+
+
