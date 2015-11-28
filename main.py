2a1132bbff61231743d313c8b5e2005f74ec0483
rasmussenj <jacob.rasmussen@students.fhnw.ch>
2015-11-28 14:54:28 +0100

    alles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pssm import *
-from roc import *
+from cutoff import *
 
 if __name__  == "__main__":
     pssm = Pssm()
@@ -11,12 +11,16 @@ if __name__  == "__main__":
     pssm.calculateNegScore()
     pssm.calculatePosScore()
 
-    roc = Roc(pssm.posScoresList, pssm.negScoresList)
-    roc.rocCurve(-1.0, 4.0, 0.1)
+#    roc = Roc(pssm.posScoresList, pssm.negScoresList)
+#    roc.rocCurve(-1.0, 4.0, 0.1)
 
-    plt.hist(pssm.negScoresList, bins=100, color="red", alpha=0.5, label="Background")
+    cutoff = CutOff(pssm.posScoresList, pssm.negScoresList)
+    cutoffValue = cutoff.getCutOff(-1, 4)
+
     plt.hist(pssm.posScoresList, bins=100, color="blue", label="SpliceSites")
-    plt.vlines(roc.cutOffValue,0,4500, colors="green")
+    plt.hist(pssm.negScoresList, bins=100, color="red", alpha=0.5, label="Background")
+    plt.vlines(cutoffValue,0,4500, colors="green")
+    # plt.vlines(roc.cutOffValue,0,4500, colors="green")
     plt.title("PSSM")
 
     plt.figure()
@@ -24,13 +28,13 @@ if __name__  == "__main__":
     plt.hist([s for s in pssm.posScoresList if s > .5], bins=100, color="blue")
     plt.title("PSSM CUT OFF")
 
-    plt.figure()
-    plt.plot(roc.falsePositiveRateList, roc.truePositiveRateList, color="purple")
-    plt.title("ROC")
+    # plt.figure()
+    # plt.plot(roc.falsePositiveRateList, roc.truePositiveRateList, color="purple")
+    # plt.title("ROC")
 
-    print "Cut Off Value:" , roc.cutOffValue
-    print "Sensitivity:" , roc.sensitivity
-    print "Specificity:" , roc.specificity
+    print "Cut Off Value:" , cutoffValue
+    # print "Sensitivity:" , roc.sensitivity
+    # print "Specificity:" , roc.specificity
 
     plt.show()
 
